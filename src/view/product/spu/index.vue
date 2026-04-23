68235c0b1502f6b4539bc549117b4d5bcd4febec
黄坤蓝 <huang2722348243@hotmail.com>
2026-04-23 16:50:08 +0800

    feat(SPU): 完成添加/修改SPU静态界面的搭建与场景切换

--- src/view/product/spu/index.vue
+++ src/view/product/spu/index.vue
@@ -4,31 +4,48 @@
     <Category :scene="scene" />
 
     <el-card style="margin-top: 15px">
-      <el-button type="primary" icon="Plus" :disabled="categoryStore.c3Id? false:true">添加SPU</el-button>
-      <el-table :data="records" style="width: 100%; margin: 15px 0" border>
-        <el-table-column label="序号" width="80px" type="index" align="center"></el-table-column>
-        <el-table-column prop="spuName" label="名称" width="180px" align="center"></el-table-column>
-        <el-table-column prop="description" label="描述" width="400px" align="center"></el-table-column>
-        <el-table-column label="操作" align="center">
-          <template #="{row,$index}">
-            <el-button type="primary" size="small" icon="Plus" title="添加SKU"/>
-            <el-button type="warning" size="small" icon="Edit" title="修改SPU"/>
-            <el-button type="info" size="small" icon="InfoFilled" title="查看SKU列表"/>
-            <el-button type="danger" size="small" icon="Delete" title="删除SPU"/>
-          </template>
-        </el-table-column>
-      </el-table>
-      <!-- 分页器 -->
-      <el-pagination
-      @size-change="sizeChange"
-      @current_change="getSPU"
-      v-model:current-page="pageNo"
-      v-model:page-size="limit"
-      :page-sizes="[3, 5, 7, 9]"
-      :background="true"
-      layout="prev, pager, next, ->, jumper, sizes, total"
-      :total="total"
-    />
+      <div v-show="scene == 0">
+        <el-button 
+          @click="addSPU"
+          type="primary" 
+          icon="Plus" 
+          :disabled="categoryStore.c3Id ? false : true"
+        >
+          添加SPU
+        </el-button>
+        <el-table :data="records" style="width: 100%; margin: 15px 0" border>
+          <el-table-column label="序号" width="80px" type="index" align="center"></el-table-column>
+          <el-table-column prop="spuName" label="名称" width="180px" align="center"></el-table-column>
+          <el-table-column
+            prop="description"
+            label="描述"
+            width="400px"
+            align="center"
+          ></el-table-column>
+          <el-table-column label="操作" align="center">
+            <template #="{ row, $index }">
+              <el-button type="primary" size="small" icon="Plus" title="添加SKU" />
+              <el-button type="warning" size="small" icon="Edit" title="修改SPU" @click="updateSPU" />
+              <el-button type="info" size="small" icon="InfoFilled" title="查看SKU列表" />
+              <el-button type="danger" size="small" icon="Delete" title="删除SPU" />
+            </template>
+          </el-table-column>
+        </el-table>
+        <!-- 分页器 -->
+        <el-pagination
+          @size-change="sizeChange"
+          @current_change="getSPU"
+          v-model:current-page="pageNo"
+          v-model:page-size="limit"
+          :page-sizes="[3, 5, 7, 9]"
+          :background="true"
+          layout="prev, pager, next, ->, jumper, sizes, total"
+          :total="total"
+        />
+      </div>
+      <!-- 添加SPU/SKU子组件 -->
+      <SpuForm v-show="scene == 1" @changeScene="changeScene"/>
+      <SkuForm v-show="scene == 2"/>
     </el-card>
   </div>
 </template>
@@ -39,6 +56,10 @@ import type { SpuResponseData, Records } from '@/api/product/spu/type'
 import { useCategoryStore } from '@/store/modules/category'
 let categoryStore = useCategoryStore()
 import { ref, watch } from 'vue'
+import SpuForm from './spuForm.vue'
+import SkuForm from './skuForm.vue'
+
+// 控制场景，0：已有的SPU，1：添加或修改SPU，2：添加SKU
 let scene = ref<number>(0)
 // 分页器默认页码
 let pageNo = ref<number>(1)
@@ -50,10 +71,13 @@ let total = ref<number>(0)
 let records = ref<Records>([])
 
 // 监听三级分类ID的变化，有变化时就可以发送请求获取数据进行展示
-watch(()=> categoryStore.c3Id, ()=>{
-  if(!categoryStore.c3Id) return
-  getSPU()
-})
+watch(
+  () => categoryStore.c3Id,
+  () => {
+    if (!categoryStore.c3Id) return
+    getSPU()
+  },
+)
 
 // 获取全部已有的SPU数据
 const getSPU = async (pager = 1) => {
@@ -62,7 +86,7 @@ const getSPU = async (pager = 1) => {
 
   let result: SpuResponseData = await reqGetSPU(pageNo.value, limit.value, categoryStore.c3Id)
   console.log(result)
-  if(result.code == 200){
+  if (result.code == 200) {
     records.value = result.data.records
   }
 }
@@ -71,6 +95,24 @@ const getSPU = async (pager = 1) => {
 const sizeChange = () => {
   getSPU()
 }
+
+// 场景0中点击添加SPU按钮的回调函数
+const addSPU = () => {
+  // 切换场景为1
+  scene.value = 1
+}
+
+// 场景0中点击修改SPU按钮的回调函数
+const updateSPU = () => {
+  // 切换场景为1
+  scene.value = 1
+}
+
+// 子组件spuForm绑定的自定义事件，用于取消按钮的切换场景功能
+const changeScene = (newScene: number) => {
+  scene.value = newScene
+}
+
 </script>
 
 <style scoped></style>
--- src/view/product/spu/spuForm.vue
+++ src/view/product/spu/spuForm.vue
@@ -1,13 +1,110 @@
 <template>
-  <div>
-    <h1>spuForm</h1>
-  </div>
+  <el-form ref="formRef" :model="form" label-width="auto">
+    <el-form-item label="名称" prop="name">
+      <el-input v-model="form.name" placeholder="请输入SPU名称" />
+    </el-form-item>
+    <el-form-item label="品牌" prop="brand">
+      <el-select v-model="form.brand" placeholder="请选择品牌">
+        <el-option label="Options1" value="1" />
+        <el-option label="Options2" value="2" />
+      </el-select>
+    </el-form-item>
+    <el-form-item label="描述" prop="desc">
+      <el-input v-model="form.desc" type="textarea" />
+    </el-form-item>
+    
+    <el-form-item label="照片" prop="images">
+      <el-upload
+        v-model:file-list="fileList"
+        action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+        list-type="picture-card"
+        :on-preview="handlePictureCardPreview"
+        :on-remove="handleRemove"
+      >
+        <el-icon><Plus /></el-icon>
+      </el-upload>
+
+      <el-dialog v-model="dialogVisible">
+        <img w-full :src="dialogImageUrl" alt="Preview Image" />
+      </el-dialog>
+    </el-form-item>
+
+    <el-form-item label="销售属性" prop="saleAttr">
+      <div style="display: flex; gap: 15px;">
+        <el-select v-model="form.saleAttr" placeholder="请选择属性">
+          <el-option label="Options1" value="1" />
+          <el-option label="Options2" value="2" />
+        </el-select>
+        <el-button type="primary" icon="Plus">添加销售属性</el-button>
+      </div>
+      <el-table :data="tableData" style="width: 100%; margin: 10px 0;" border>
+        <el-table-column type="index" label="序号" width="80px" align="center" />
+        <el-table-column prop="attrName" label="属性名" width="180px" align="center" />
+        <el-table-column prop="attrValue" label="属性值" align="center" />
+        <el-table-column label="操作" width="80px" align="center">
+          <template #default>
+            <el-button type="danger" size="small" icon="Delete" link />
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-form-item>
+
+    <el-form-item>
+      <el-button type="primary">保存</el-button>
+      <el-button @click="cancel">取消</el-button>
+    </el-form-item>
+  </el-form>
 </template>
 
 <script setup lang="ts">
+import { ref, reactive } from 'vue'
+import { Plus } from '@element-plus/icons-vue'
+import type { UploadProps, UploadUserFile } from 'element-plus'
 
+let $emit = defineEmits(['changeScene'])
+// 点击取消按钮，通知父组件切换场景
+const cancel = () => {
+  $emit('changeScene', 0)
+}
+// 表单数据
+const form = reactive({
+  name: '',
+  brand: '',
+  desc: '',
+  saleAttr: '',
+  images: []
+})
+
+// 文件列表（示例数据）
+const fileList = ref<UploadUserFile[]>([
+  {
+    name: 'food.jpeg',
+    url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100',
+  },
+])
+
+// 图片预览
+const dialogImageUrl = ref('')
+const dialogVisible = ref(false)
+
+const handleRemove: UploadProps['onRemove'] = (uploadFile, uploadFiles) => {
+  console.log('删除文件:', uploadFile, uploadFiles)
+}
+
+const handlePictureCardPreview: UploadProps['onPreview'] = (uploadFile) => {
+  dialogImageUrl.value = uploadFile.url!
+  dialogVisible.value = true
+}
+
+// 表格数据
+const tableData = ref([
+  { attrName: '颜色', attrValue: '黑色,白色,红色' },
+  { attrName: '尺寸', attrValue: 'S,M,L,XL' }
+])
 </script>
 
 <style scoped lang="scss">
-
+.el-select {
+  width: 200px;
+}
 </style>
