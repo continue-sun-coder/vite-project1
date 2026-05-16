334b936d7890d92414327484f3e924ff95c7da20
黄坤蓝 <huang2722348243@hotmail.com>
2026-05-16 22:38:17 +0800

    feat(Layout): 完成主题颜色修改功能

--- src/layout/header/theme/index.vue
+++ src/layout/header/theme/index.vue
@@ -97,6 +97,32 @@ const changeDark = () => {
     html.classList.remove('dark')
   }
 }
+
+// 颜色组件数据
+const color = ref('rgba(255, 69, 0, 0.68)')
+const predefineColors = ref([
+  '#ff4500',
+  '#ff8c00',
+  '#ffd700',
+  '#90ee90',
+  '#00ced1',
+  '#1e90ff',
+  '#c71585',
+  'rgba(255, 69, 0, 0.68)',
+  'rgb(255, 120, 0)',
+  'hsv(51, 100, 98)',
+  'hsva(120, 40, 94, 0.5)',
+  'hsl(181, 100%, 37%)',
+  'hsla(209, 100%, 56%, 0.73)',
+  '#c7158577',
+])
+
+// 主题颜色设置
+const setColor = () => {
+  // 通过js修改根节点的样式属性与属性值
+  const html = document.documentElement
+  html.style.setProperty('--el-color-primary', color.value)
+}
 </script>
 
 <style scoped lang="scss">
